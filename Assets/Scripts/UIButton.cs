5f8fdc6564d99f353c0b8292983a089a97ba98ca
Faqan <43637302+Faqan086@users.noreply.github.com>
2021-02-12 00:30:21 +0400

    Files

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject;
+    [SerializeField] private string targetMessage;
+    public Color highlightColor = Color.cyan;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void OnMouseOver()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = highlightColor;
+        }
+    }
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        }
+    }
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+    }
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+        if (targetObject != null)
+        {
+            targetObject.SendMessage(targetMessage);
+        }
+    }
+    public void Restart()
+    {
+        Application.LoadLevel("Scene");
+    }
+}
